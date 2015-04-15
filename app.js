cdab7c27c28ded7e3aca61e98cd82c1bbdae0912
Nickolay Luschitsky <schCRABicus@gmail.com>
2015-04-15 07:32:15 +0300

    - favicon added

--- app.js
+++ app.js
@@ -9,29 +9,30 @@ var
     , routes = require('./routes/index')
     , users = require('./routes/users')
 
-    , app = express();
+    , app = express()
+    ;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app
+    .use(favicon(__dirname + '/public/favicon.ico'))
+    .use(logger('dev'))
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(cookieParser())
+    .use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', routes);
-app.use('/users', users);
+    .use('/', routes)
+    .use('/users', users)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    // catch 404 and forward to error handler
+    .use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
 
 // error handlers
 
